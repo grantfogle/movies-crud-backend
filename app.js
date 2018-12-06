4e91f65cad86b1b4f44e8083559c1b94630e08b0
grantfogle <grantjfogle@gmail.com>
2018-12-06 11:59:41 -0700

    added all routes and the are working

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
-
 const knex = require('knex')
 const queries = require('./queries');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser());
+app.use(cors())
 
 app.get('/', (req, res) => {
     queries.listAll().then(movies => res.send(movies));
 })
 
+app.get('/:id', (req, res) => {
+    queries.getById(req.params.id).then(movies => res.send(movies))
+})
+
+app.post('/', (req, res) => {
+    queries.createMovie(req.body).then(movies => res.send(movies))
+})
+
+app.delete('/:id', (req, res) => {
+    queries.deleteMovie(req.params.id).then(movies => res.send({ status: 204, message: "it has been deleted" }))
+})
+
+app.put('/:id', (req, res) => {
+    queries.updateMovie(req.params.id, req.body).then(updatedMovie => res.json(updatedMovie[0]))
+})
 
 const listener = () => console.log(`Listening on port ${port}`);
 app.listen(port, listener);
--- queries.js
+++ queries.js
@@ -3,5 +3,21 @@ const database = require('./database-connection');
 module.exports = {
     listAll() {
         return database('movies');
+    },
+    getById(id) {
+        return database('movies').where('id', id);
+    },
+    createMovie(newMovie) {
+        return database('movies').insert(newMovie).returning('*');
+    },
+    deleteMovie(id) {
+        return database('movies')
+            .where('id', id)
+            .delete()
+    },
+    updateMovie(id, movie) {
+        return database('movies')
+            .where('id', id)
+            .update(movie)
     }
 }
