9697c541b37f51323040774d6203ebe403f6095b
grantfogle <grantjfogle@gmail.com>
2018-12-06 10:26:40 -0700

    added first get route

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.listen(console.log(`Listening on ${port}`));
+app.get('/', (req, res) => {
+    res.send('yea buddy');
+})
+
+const listener = () => console.log(`Listening on port ${port}`);
+app.listen(port, listener);
