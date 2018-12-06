5a98b152b305fbf6e7e7fdb3c6ea8b967024bafa
grantfogle <grantjfogle@gmail.com>
2018-12-06 11:12:02 -0700

    added queries file and local host is displaying data

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
+const knex = require('knex')
+const queries = require('./queries');
+
 app.get('/', (req, res) => {
-    res.send('yea buddy');
+    queries.listAll().then(movies => res.send(movies));
 })
+
 
 const listener = () => console.log(`Listening on port ${port}`);
 app.listen(port, listener);
